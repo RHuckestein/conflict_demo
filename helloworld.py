5c9f6c18cce8f9768c3eca40db9b90f2abb9db54
Jake Huckestein <jhuckestein@outlook.com>
2022-04-25 13:40:46 -0400

    add city

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,3 @@
-<< << << < HEAD
-
 
 def hello_world(state):
     print(f'Hello World {state}!')
@@ -9,19 +7,5 @@ def main():
     hello_world('New York')
 
 
-== == == =
 
 
-def hello_world(city, state):
-    print(f'Hello World {city}, {state}!')
-
-
-def main():
-    hello_world('NYC', 'New York')
-
-
-main()
->>>>>> > 896ee918b0ef811797de16ea0cb04a52a331d64e
-
-
-main()
